57ea90f6c53811d4ef9dc3755299c98bd735e56f
Erick <erickgarcia200811@gmail.com>
2025-12-22 22:21:14 -0600

    Productos individuales

--- paginas/Productos/productos.js
+++ paginas/Productos/productos.js
@@ -4,6 +4,44 @@ const productos = [
         descripcion: "Descripción del producto",
         precio: 50,
         imagen: "../../assets/shine.jpeg"
+
+
+    },
+    {
+        nombre: "Producto 2",
+        descripcion: "Descripción del producto",
+        precio: 75,
+        imagen: "../../assets/shampoo.jpeg"
+    },
+    {
+        nombre: "Producto 3",
+        descripcion: "Descripción del producto",
+        precio: 100,
+        imagen: "../../assets/innove.jpeg"
+    },
+    {
+        nombre: "Producto 4",
+        descripcion: "Descripción del producto",
+        precio: 120,
+        imagen: "../../assets/shampoo2.jpeg"   
+    },
+    {
+        nombre: "Producto 5",
+        descripcion: "Descripción del producto",
+        precio: 235,
+        imagen: "../../assets/acondicionador.jpeg"
+    },
+    {
+        nombre: "Producto 6",
+        descripcion: "Descripción del producto",
+        precio: 150,
+        imagen: "../../assets/Skala.jpeg"
+    },
+    {
+        nombre: "Producto 7",
+        descripcion: "Descripción del producto",
+        precio: 180,
+        imagen: "../../assets/rizos.jpeg"
     }
 ];
 
