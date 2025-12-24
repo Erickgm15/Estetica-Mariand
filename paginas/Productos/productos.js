8ab93bcf1198c0fa5210bb7d78fea9faa456b327
Erick <erickgarcia200811@gmail.com>
2025-12-23 18:06:23 -0600

    prodcutos hasta el 100

--- paginas/Productos/productos.js
+++ paginas/Productos/productos.js
@@ -72,56 +72,56 @@ const productos = [
     {
         nombre: "Producto 12",
         descripcion: "Descripción del producto",
-        precio: 140,
-        imagen: "../../assets/gel.jpeg"
+        precio: 320,
+        imagen: "../../assets/hydra.jpeg"
     },
     {
         nombre: "Producto 13",
         descripcion: "Descripción del producto",
         precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        imagen: "../../assets/fluid.jpeg"
     },
     {
         nombre: "Producto 14",
         descripcion: "Descripción del producto",
-        precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        precio: 110,
+        imagen: "../../assets/tratamiento.jpeg"
     },
     {
         nombre: "Producto 15",
         descripcion: "Descripción del producto",
         precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        imagen: "../../assets/splite.jpeg"
     },
     {
         nombre: "Producto 16",
         descripcion: "Descripción del producto",
         precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        imagen: "../../assets/defy.jpeg"
     },
     {
         nombre: "Producto 17",
         descripcion: "Descripción del producto",
         precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        imagen: "../../assets/leave.jpeg"
     },
     {
         nombre: "Producto 18",
         descripcion: "Descripción del producto",
-        precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        precio: 75,
+        imagen: "../../assets/peinarcrema.jpeg"
     },
     {
         nombre: "Producto 19",
         descripcion: "Descripción del producto",
-        precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        precio: 150,
+        imagen: "../../assets/capilar.jpeg"
     },
     {
         nombre: "Producto 20",
         descripcion: "Descripción del producto",
         precio: 160,
-        imagen: "../../assets/cremapeinar.jpeg"
+        imagen: "../../assets/ins.jpeg"
     }
 ];
 
@@ -130,23 +130,84 @@ const productos = [
 ========================= */
 
 const preciosExtra = [
-    170, 185, 160, 200, 175, 190, 165, 210, 180, 155
+    17, 20, 160, 200, 175, 190, 165, 210, 180, 110,
+    150,150,400,40,89,35,75,200,110,110,400,23,188,35,
+    24,56,23,56,78,65,78,34,150,150,23,75,65,45,20,
+    67,23,67,4,89,170,23,45,678,150,300,220,194,78,120,
+    200,220,175,226,282,175,175,196,220,210,175,
 ];
 
 const imagenesExtra = [
-    "../../assets/p21.jpeg",
-    "../../assets/p22.jpeg",
-    "../../assets/p23.jpeg",
-    "../../assets/p24.jpeg",
-    "../../assets/p25.jpeg",
-    "../../assets/p26.jpeg",
-    "../../assets/p27.jpeg",
-    "../../assets/p28.jpeg",
-    "../../assets/p29.jpeg",
-    "../../assets/p30.jpeg"
+    "../../assets/crono.jpeg",
+    "../../assets/restru.jpeg",
+    "../../assets/repair.jpeg",
+    "../../assets/minuti.jpeg",
+    "../../assets/argan.jpeg",
+    "../../assets/acido.jpeg",
+    "../../assets/insi2.jpeg",
+    "../../assets/splash.jpeg",
+    "../../assets/color.jpeg",
+    "../../assets/cabellorizo.jpeg",
+    "../../assets/definicion.jpeg",
+    "../../assets/coat.jpeg",
+    "../../assets/argan2.jpeg",
+    "../../assets/oil.jpeg",
+    "../../assets/blonde.jpeg",
+    "../../assets/repair2.jpeg",
+    "../../assets/locion.jpeg",
+    "../../assets/abrillantador.jpeg",
+    "../../assets/tomateke.jpeg",
+    "../../assets/shine2.jpeg",
+    "../../assets/marula.jpeg",
+    "../../assets/crono2.jpeg",
+    "../../assets/thermal.jpeg",
+    "../../assets/forme308.jpeg",
+    "../../assets/shampomoi.jpeg",
+    "../../assets/move.jpeg",
+    "../../assets/mascarilla150.jpeg",
+    "../../assets/line.jpeg",
+    "../../assets/abril.jpeg",
+    "../../assets/yellow.jpeg",
+    "../../assets/cellinove.jpeg",
+    "../../assets/hair.jpeg",
+    "../../assets/skalaninas.jpeg",
+    "../../assets/skalamandarina.jpeg",
+    "../../assets/hair2.jpeg",
+    "../../assets/restruc.jpeg",
+    "../../assets/suero.jpeg",
+    "../../assets/densi.jpeg",
+    "../../assets/hydrasplash.jpeg",
+    "../../assets/makeme.jpeg",
+    "../../assets/brazilian.jpeg",
+    "../../assets/mascarillafortal.jpeg",
+    "../../assets/shampoocare.jpeg",
+    "../../assets/molecular.jpeg",
+    "../../assets/jengibre.jpeg",
+    "../../assets/chapumenergi.jpeg",
+    "../../assets/skalababosa.jpeg",
+    "../../assets/reparative.jpeg",
+    "../../assets/119.jpeg",
+    "../../assets/silver.jpeg",
+    "../../assets/yellowcurls.jpeg",
+    "../../assets/yellowoil.jpeg",
+    "../../assets/hair3.jpeg",
+    "../../assets/mask.jpeg",
+    "../../assets/mouisture.jpeg",
+    "../../assets/yellowlis.jpeg",
+    "../../assets/semidelino.jpeg",
+    "../../assets/yellow2.jpeg",
+    "../../assets/spray.jpeg",
+    "../../assets/keratin.jpeg",
+    "../../assets/silverh.jpeg",
+    "../../assets/keratin2.jpeg",
+    "../../assets/yellow3.jpeg",
+    "../../assets/yellowmask.jpeg",
+    "../../assets/firenze.jpeg",
+
+
 ];
 
-for (let i = 21; i <= 193; i++) {
+for (let i = 21; i <= 100; i++) {
     const index = (i - 21) % preciosExtra.length;
 
     productos.push({
